5a7b7158827cdceb1e5fa3cc2833820a99f09e3e
usamaidrsk <53287480+usamaidrsk@users.noreply.github.com>
2019-12-11 14:12:12 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,6 @@ import React, {Component} from 'react';
 export default class App extends Component{
 	render (){
 		return ()
-		{ <p>This is my new react app }
+		{ <p>This is my new react app </p>}
 	}
 }
